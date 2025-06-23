5a1da9bb45f4963a85f64418a086fdddc61b9d00
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-06-23 00:23:19 -0300

    Validações e exibição de notifiação

--- app.py
+++ app.py
@@ -14,25 +14,39 @@ def main(page: ft.Page):
     lista_produtos = ft.ListView()
 
     def cadastrar(e):
-        novo_produto = Produto(
+        try:
+            if not produto.value or not preco.value:
+                raise ValueError("Os campos não podem estar vazios.")
+            float(preco.value) 
+            
+            novo_produto = Produto(
             titulo=produto.value,
             preco=float(preco.value)
-        )
-        session.add(novo_produto)
-        session.commit()
-        lista_produtos.controls.append(ft.Container(
-                ft.Text(produto.value + f' - R$ {preco.value}'),
-                bgcolor=ft.Colors.BLACK12,
-                padding=15,
-                alignment=ft.alignment.center,
-                margin=3,
-                border_radius=10
-            ))
+            )
+            session.add(novo_produto)
+            session.commit()
+            lista_produtos.controls.append(ft.Container(
+                    ft.Text(produto.value + f' - R$ {preco.value}'),
+                    bgcolor=ft.Colors.BLACK12,
+                    padding=15,
+                    alignment=ft.alignment.center,
+                    margin=3,
+                    border_radius=10
+                ))
+            txt_erro.visible = False
+            txt_acerto.visible = True
+
+            produto.value = ''
+            preco.value = 0
+        except:
+            txt_erro.visible = True
+            txt_acerto.visible = False 
+
         page.update()
         print(f'Produto {novo_produto.titulo} cadastrado com sucesso!')
 
-    txt_erro = ft.Container(ft.Text('Erro ao salkvar o produto!'), bgcolor=ft.Colors.RED, padding=10, visible=False, alignment=ft.alignment.center)
-    txt_erro = ft.Container(ft.Text('Produto salvo com sucesso!'), bgcolor=ft.Colors.RED, padding=10, visible=False, alignment=ft.alignment.center)
+    txt_erro = ft.Container(ft.Text('Erro ao salvar o produto!'), bgcolor=ft.Colors.RED, padding=10, visible=False, alignment=ft.alignment.center)
+    txt_acerto = ft.Container(ft.Text('Produto salvo com sucesso!'), bgcolor=ft.Colors.GREEN, padding=10, visible=False, alignment=ft.alignment.center)
 
     txt_titulo = ft.Text('Cadastrar novo produto') 
     produto = ft.TextField(label='Digite o titulo do  produto...', text_align=ft.TextAlign.LEFT)
@@ -41,6 +55,8 @@ def main(page: ft.Page):
     btn_produto = ft.ElevatedButton('Cadastrar', on_click=cadastrar)
 
     page.add(
+        txt_acerto,
+        txt_erro,
         txt_titulo,
         produto,
         txt_preco,
